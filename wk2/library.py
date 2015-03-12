f061ebe5b93d1e45a94e2157f875c0702d949f21
icarlosmendez <calicci.gardachi@gmail.com>
2015-03-12 08:32:16 -0400

    commenting code

--- wk2/library.py
+++ wk2/library.py
@@ -2,55 +2,64 @@
 class Student:
     # initialize the constructor function
     def __init__(self):
-        print 'Student initialized'
         self.make_student()
 
 
     def make_student(self):
         # create a variable for new student
         self.new_student = []
-        self.average = []
-        # define the attributes
 
+        # define the basic attributes
         self.name = raw_input('Student name: ')
         self.degree = raw_input('Student degree: ')
-        # self.grades = [90, 50, 100]
+
+        # grades is a list populated by a for loop
+        # user input is collected using raw_input
         self.grades = []
         for i in range(1, 4):
             self.grades.append(raw_input('Enter a grade: '))
+
+        # average uses the grades variable to output the GPA
+        self.average = []
         self.average = self.avg(self.grades)
 
+        # append all relevant inputs to new_student list
         self.new_student.append(self.name)
         self.new_student.append(self.degree)
         self.new_student.append(self.average)
 
 
-    def get_student_info(self):
+    def get_student_data(self):
+        # make new_student available to other functions
+        # in this case that is the Printer class
         return self.new_student
 
 
     # create a method to calculate average grade
     def avg(self, grades):
-        # create a variable to hold the sum of all grades
+        # create a variable to hold the output of avg()
         average = 0
         # iterate over list
         for i in self.grades:
-            # add each new index value to variable
+            # add each new index value to variable average
+            # convert i to an integer each time before adding to average
             average += int(i)
-        # create another variable to assign the average to
+        # calculate the average and reassign new value to variable average
         average = average/len(self.grades)
         # return new variable with average grade
-        # print "%s's average is " + str(average) % self.name
         return average
 
 
 class Printer:
     # initialize Printer class constructor function
     def __init__(self):
-        print 'Printer initialized'
+        # data is a variable created in the controller
+        # data is the culmination of make_student and get_student_data
+        # establishing the variable of data provides a location for
+        # the values from new_student to be passed into the Printer class
         self.data = None
 
 
     def print_out(self):
-        # print individual attributes from Student class
-        print '%s who is enrolled in %s degree program has a GPA of %s.' % (self.data[0], self.data[1], self.data[2])
+        # print individual attributes from variable new_student
+        print '%s who is enrolled in the %s degree program has earned a GPA of %s.' % (self.data[0], self.data[1], self.data[2])
--- wk2/main.py
+++ wk2/main.py
@@ -1,14 +1,21 @@
 from library import Student, Printer
 
-class MainHandler:
+class Controller:
     def __init__(self):
-        print 'MainHandler initialized'
 
+        # define the variable model as Student class function call
         model = Student()
+        # define the variable view as Printer class function call
         view = Printer()
-        data = model.get_student_info()
+
+        # data receives the results of the make_student function
+        # the variable new_student is returned by get_student_data
+        data = model.get_student_data()
+        # assign the value 'data' in the Printer class
+        # the results of get_student_data()
         view.data = data
+        # call the print_out() function of the Printer class
         view.print_out()
 
-main = MainHandler()
+controller = Controller()
 
