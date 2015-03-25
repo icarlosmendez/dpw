ac542b5f6ddec5254b19818c480223d80dc7885d
icarlosmendez <calicci.gardachi@gmail.com>
2015-03-24 23:53:18 -0400

    not so simple form

--- appengine/form/controller.py
+++ appengine/form/controller.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 import webapp2, os, jinja2
-from data_model import Form
-from data_view import View
 
 
 jinja2_environment = jinja2.Environment(
@@ -13,24 +11,37 @@ jinja2_environment = jinja2.Environment(
 
 
 class Controller(webapp2.RequestHandler):
-    print '2: MainHandler initialized'
+    print '1: Controller initialized'
     def get(self):
-
-        model = Form()
-        view = View()
 
         template_values = {
             'Welcome': 'Welcome to Carib'
         }
-        self.template = jinja2_environment.get_template('form.html')
+        template = jinja2_environment.get_template('form.html')
 
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(self.template.render(template_values))
+        self.response.write(template.render(template_values))
+        self.response.write(template)
 
-        self.response.write(self.template)
-        # self.response.write(view.review(model.do))
-        self.response.write(model.do)
 
+        if self.request.GET:
+            companyName = self.request.GET['companyName']
+            url = self.request.GET['url']
+            launchDate = self.request.GET['launchDate']
+            priName = self.request.GET['priName']
+            priEmail = self.request.GET['priEmail']
+            priPhone = self.request.GET['priPhone']
+            priRole = self.request.GET['priRole']
+            secName = self.request.GET['secName']
+            secEmail = self.request.GET['secEmail']
+            secPhone = self.request.GET['secPhone']
+            secRole = self.request.GET['secRole']
+
+            self.response.write(companyName + url + launchDate + priName + priEmail + priPhone + priRole + secName + secEmail + secPhone + secRole)
+
+        else:
+            pass
+            self.response.write(template)
 
 
 
--- appengine/form/data_model.py
+++ appengine/form/data_model.py
@@ -1,46 +1,60 @@
-import webapp2, cgi
+# import webapp2
 
-class Form(object):
-    print '3: Model initialized'
-    def __init__(self):
-        self.do = FormDO()
-
-        # instantiate FieldStorage function from the cgi class/module and set it to form
-        # self.do.form = cgi.FieldStorage()
+# class Form(object):
+#     print '3: Model initialized'
+#     def __init__(self):
+#         self.do = FormDO()
 
 
 
-class FormHandler(webapp2.RequestHandler, Form):
-    def __init__(self):
-        super(Form, self).__init__()
+# class FormHandler(webapp2.RequestHandler, Form):
+#     def __init__(self):
+#         super(Form, self).__init__()
+#
+#     def get(self):
+#         if self.request.GET:
+#             self.do.companyName = self.request.GET['companyName']
+#             self.do.url = self.request.GET['url']
+#             self.do.launchDate = self.request.GET['launchDate']
+#             self.do.priName = self.request.GET['priName']
+#             self.do.priEmail = self.request.GET['priEmail']
+#             self.do.priPhone = self.request.GET['priPhone']
+#             self.do.priRole = self.request.GET['priRole']
+#             self.do.secName = self.request.GET['secName']
+#             self.do.secEmail = self.request.GET['secEmail']
+#             self.do.secPhone = self.request.GET['secPhone']
+#             self.do.secRole = self.request.GET['secRole']
+#
+#         else:
+#             pass
+#             self.response.write(template)
 
-    def get(self):
-        if self.request.GET:
-            # capture company name
-            self.do.companyName = self.request.GET['companyName']
-            self.do.url = self.request.GET['url']
-            self.do.launchDate = self.request.GET['launchDate']
-            # prevent script injection by escaping the user input
-            # self.do.companyName = cgi.escape(self.do.companyName)
-
-        else:
-            pass
-            # self.response.write(template)
 
 
+# class FormDO():
+#     print '4: FormDO initialized'
+#     def __init__(self):
+#         self.form = []
+#         self.companyName = ''
+#         self.url = ''
+#         self.launchDate = ''
+#         self.priName = ''
+#         self.priEmail = ''
+#         self.priPhone = ''
+#         self.priRole = ''
+#         self.secName = ''
+#         self.secEmail = ''
+#         self.secPhone = ''
+#         self.secRole = ''
 
-class FormDO():
-    print '4: FormDO initialized'
-    def __init__(self):
-        self.form = []
-        self.companyName = ''
-        self.url = ''
-        self.launchDate = ''
-        self.priName = ''
-        self.priEmail = ''
-        self.priPhone = ''
-        self.priRole = ''
-        self.secName = ''
-        self.secEmail = ''
-        self.secPhone = ''
-        self.secRole = ''
+
+
+# class View():
+#     def __init__(self):
+#         print '5: View initialized'
+#         pass
+#
+#     def review(self, do):
+#         print '6: review initialized'
+#         for attr, value in do.__dict__.iteritems():
+#             print attr, value
